8795cde51b112c01cd59ee73fd0910432703bdf9
Mai Van Manh <maivanmanh@tdtu.edu.vn>
2025-11-23 15:02:38 +0700

    Add MVM prebuilt binary formula v1.0.0

---
+++ Formula/mvm.rb
@@ -0,0 +1,16 @@
+class MVM < Formula
+  desc "A simple prebuilt C program"
+  homepage "https://github.com/maivanmanh/homebrew-private-tap"
+  url "https://github.com/maivanmanh/homebrew-private-tap/releases/download/v1.0.0/main-1.0.0.tar.gz"
+  sha256 "72120cc1d9dfeb4bab35ff0f816c29a80aae348f61310de169dac0623c418df8"
+  license "MIT"
+
+  def install
+    bin.install "mvm"
+  end
+
+  test do
+    system "#{bin}/mvm", "--help"
+  end
+end
+
